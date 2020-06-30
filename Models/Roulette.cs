d3222b30e248502085c5fbf5670569c61bb0524e
Guillermo Galindo <guillermoplus@hotmail.com>
2020-06-30 11:00:01 -0500

    Bet and Roulette models created

---
+++ Models/Roulette.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BettingRoulette
+{
+    public class Roulette
+    {
+        public long Id { get; set; }
+        public string Status { get; set; }
+        public Bet Bets { get; set; }
+
+        public Roulette()
+        {
+
+        }
+    }
+}
